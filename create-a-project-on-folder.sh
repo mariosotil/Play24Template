6695f78430f4c39b8d23ae6b38d74192d9f70df6
mariosotil <mario.sotil@gmail.com>
2016-06-29 10:56:18 -0400

    Improving the script for create new projects

--- create-a-project-on-folder.sh
+++ create-a-project-on-folder.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+this_script=`basename "$0"`
+
+source="`pwd`/"
 target=$1
-me=`basename "$0"`
-folder=`pwd`
-mkdir $target
-rsync -av "$folder/" "$target"
-rm -f "$target/$me"
+
+read -p "Copying from $source to $target. Do you want to continue? [Y/n] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Check if the folder exists
+if [[ -e $target ]]; then
+    echo "$target already exists" 1>&2
+    exit 99
+fi
+
+# Copy everything...
+rsync -av "$source" "$target"
+
+# Except this script
+rm -f "$target/$this_script"
